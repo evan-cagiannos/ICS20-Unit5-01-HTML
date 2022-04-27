f8969f571dbc15f1f445799397600c440f2e1c84
Evan Cagiannos <99355231+evan-cagiannos@users.noreply.github.com>
2022-04-27 23:50:25 +0000

    FIXES

--- js/script.js
+++ js/script.js
@@ -1,29 +1,46 @@
-// Copyright (c) 2022 Evan Cagiannos ALL rights reserved
-//
-// Created by: Evan Cagiannos
-// Created on: April 2022
-// This file contains the JS functions for index.html
+/* Created by: Evan Cagiannos
+   Created on: April 2022
+   This file contains JS functions for index.html*/
+
+"use strict"
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit5-01-HTML/sw.js", {
-    scope: "/ICS2O-Unit5-01-HTML/",
-  });
+ if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-UNIT-5-01-HTML/sw.js", {
+    scope: "/ICS2O-UNIT-5-01-HTML/",
+  })
 }
+/**
+ * This function generates a random number
+ */
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
 
 /**
  * This function updates the slider value.
  */
-function updateSliderValue(valueFromSlider) {
-  document.getElementById("slider-value").innerHTML = valueFromSlider;
+ function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
 }
 
+console.log(randomNumber)
+
 /**
- * This function displays the slider value.
+ * This function compares slider with random number.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML =
-    "<p>Value is: " + slider.value + "</p>";
+function buttonClicked() {
+  const sliderValue = slider.value
+
+  
+  if (sliderValue == randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "Answer is " + randomNumber + ". Correct!"
+  }
+  //  block of code to be executed if slider is != to randomNumber
+  if (sliderValue != randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "Answer is " + randomNumber + ". Incorrect..."
+  }
 }
